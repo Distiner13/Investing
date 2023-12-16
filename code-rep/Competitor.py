bb0397e2849da83385277768c4f88a4e1ed5f0b9
Anis Mansour <anismansour13.11@gmail.com>
2023-12-16 15:16:57 -0500

    Just tried to fix Competitor since the class name for the table was modifed since july when we first wrote this

--- code-rep/Competitor.py
+++ code-rep/Competitor.py
@@ -1,3 +1,4 @@
+#%%
 import re
 import requests
 import pandas as pd
@@ -21,9 +22,14 @@ def getCompetitors(symbol):
     while(requests.get("https://finviz.com/screener.ashx?v=152&f=ind_" + industry + ",sec_" + sector + "&r=" + str(k) + "1&c=0,1,2,79,3,4,5,6,7,8,9,10,11,12,13,73,74,75,14,15,16,77,17,18,19,20,21,23,22,82,78,24,25,85,26,27,28,29,30,31,84,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,68,70,80,83,76,60,61,62,63,64,67,69,81,65,66", headers=headers)):
         url = "https://finviz.com/screener.ashx?v=152&f=ind_" + industry + ",sec_" + sector + "&r=" + str(k) + "1&c=0,1,2,79,3,4,5,6,7,8,9,10,11,12,13,73,74,75,14,15,16,77,17,18,19,20,21,23,22,82,78,24,25,85,26,27,28,29,30,31,84,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,68,70,80,83,76,60,61,62,63,64,67,69,81,65,66"
         print(url)
-        response = requests.get(url, headers=headers)
+        header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+        # could use pandas to extract the table instead of beautiful soup
+        comparison = pd.read_html(url, headers=header)
+        len(comparison)
+
+        '''response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.text, features="html.parser")
-        table = soup.find('table', class_= 'table-light is-new')
+        table = soup.find('table', class_= 'styled-table-new is-rounded is-tabular-nums w-full screener_table')
         trs = table.find_all('tr')
         col = []
         values = []
@@ -42,9 +48,9 @@ def getCompetitors(symbol):
                    list = data[1:].split("!")
                    values.append(list)
            except:
-               pass
+               pass'''
 
-        comparison = pd.DataFrame(values, columns=col)
+        #comparison = pd.DataFrame(values, columns=col)
         if (len(comparison) == 1):
             break
         print(comparison)
@@ -74,4 +80,4 @@ def getCompetitors(symbol):
         k += 2
 
 
-#getCompetitors("ADN")
+getCompetitors("ADN")
